be559516998db5e40df1d656752b23ee6c243dcc
AlenaSemenova <alenasemenova@list.ru>
2016-04-20 00:11:31 +0300

    Класс планет

--- Game/Game/Planets.h
+++ Game/Game/Planets.h
@@ -1,14 +1,20 @@
-#pragma once
+﻿#pragma once
 #include <SFML\Graphics.hpp>
 using namespace std;
 using namespace sf;
 
-class Planets
+class Planets: public Sprite
 {
 public:
-	Planets();
-	Vector2f position;
-	float radius;
+	/* Конструктор: устанавливает расположение, масштабирование(!), массу, скорость, ускорение, текстуру */
+	Planets(Vector2f m_pos, Vector2f m_scale, float m_mass, float m_vel, float m_acc, Texture m_texture) : Sprite(m_texture)
+	{
+		setPosition(m_pos);
+		setScale(m_scale);
+		mass = m_mass;
+		velocity = m_vel;
+		acceleration = m_acc;
+	}
 	float mass;
 	float velocity;
 	float acceleration;
