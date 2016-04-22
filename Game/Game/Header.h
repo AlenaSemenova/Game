537dce5da181e9d0c6985753b813910cd07e8e38
AlenaSemenova <alenasemenova@list.ru>
2016-04-22 23:29:51 +0300

    Game

--- Game/Game/Header.h
+++ Game/Game/Header.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <map>
 #include <SFML\Graphics.hpp>
+#include <thread>
+#include <atomic>
+#include <mutex>
 
 using namespace sf;
 using namespace std;
@@ -11,6 +14,3 @@ using namespace std;
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGTH 600
-// время в миилисекуднах для обновления расположения планет//
-#define time_step 40
-
--- Game/Game/main.cpp
+++ Game/Game/main.cpp
@@ -1,28 +1,62 @@
 ﻿#include "Header.h"
 
+#define drawing_step 40
+#define calculation_step 20
+
+mutex _mutex;
+float dt;
+
+/*  Нить вычислений.
+Надо бы подумать, когда она закнчивает работу */
+void CalculationThread(PhysicsEngine & planets)
+{
+	Clock calculation_clock;
+	while(1)
+	{
+		/* Время обновления координат - 20 миллисекунд */
+		if (calculation_clock.getElapsedTime().asMilliseconds() > calculation_step)
+		{
+			_mutex.lock();
+			planets.UpdatePosition();
+			_mutex.unlock();
+		}
+	}
+		
+}
+
 int main()
 {
 	map <int, Planet> planets_drawing;
-	PhysicsEngine planets_calculation = PhysicsEngine();
+	PhysicsEngine planets_calculation;
 	planets_calculation.Initialize();
+	planets_calculation.GetSnapshot(planets_drawing);
+	thread Thread(CalculationThread, planets_calculation);
 
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGTH), "Game");
-	Clock clock;
+	Clock elapsed_time, drawing_clock;
+	Event event;
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		/* Шаг отрисовки - 40 миллисекунд */
+		if (drawing_clock.getElapsedTime().asMilliseconds() > drawing_step)
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+			drawing_clock.restart();
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
 
-		if (clock.getElapsedTime().asMilliseconds() > time_step)
-		{
-			clock.restart();
+			/* Обновляем расположение планет */
+			_mutex.lock();
+			planets_calculation.GetSnapshot(planets_drawing);
+			dt = elapsed_time.getElapsedTime().asSeconds(); //записываем точное прошедшее время в секундах (нужно для вычислений)
+			elapsed_time.restart();
+			_mutex.unlock();
+			
+			window.clear();
+			window.display();
 		}
-
-		window.clear();
-		window.display();
 	}
+		
 }
